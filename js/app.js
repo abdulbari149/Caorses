7707015dc85288d6e3f2a7b9f37e20253a65cdcb
abdulbari149 <abdulbari131103@gmail.com>
2020-07-23 13:05:38 +0500

    all files

---
+++ js/app.js
@@ -0,0 +1,6 @@
+let header = document.querySelector("#c-head");
+document.addEventListener('scroll', ()=>{
+    header.classList.add('left');
+})
+header.classList.remove('left')
+document.addEventListener("")
