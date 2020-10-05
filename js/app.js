f3f63664ca60da608a7c037c16e204690b99a71f
abdulbari149 <abdulbari131103@gmail.com>
2020-10-06 00:55:41 +0500

    reawakening the website

--- js/app.js
+++ js/app.js
@@ -2,5 +2,9 @@ let header = document.querySelector("#c-head");
 document.addEventListener('scroll', ()=>{
     header.classList.add('left');
 })
-header.classList.remove('left')
+header.classList.remove('left');
+
+
+
+
 
